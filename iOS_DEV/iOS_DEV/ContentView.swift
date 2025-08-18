2bd0b5a0f00492d39888f30f020cf149ca99e5e9
jhyoo93 <167679028+jhyoo93@users.noreply.github.com>
2025-08-18 21:08:16 +0900

    프로젝트 생성

---
+++ iOS_DEV/iOS_DEV/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_DEV
+//
+//  Created by 유재환 on 8/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_DEV/iOS_DEV/DemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DEVApp.swift
+//  iOS_DEV
+//
+//  Created by 유재환 on 8/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
