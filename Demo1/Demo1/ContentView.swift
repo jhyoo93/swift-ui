02ea7e63d7f2aa97077018b8c001f5359f13a7ef
jhyoo93 <167679028+jhyoo93@users.noreply.github.com>
2025-08-22 16:19:59 +0900

    프로젝트 설정

---
+++ Demo1/Demo1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demo1
+//
+//  Created by 유재환 on 8/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo2/Demo2/Demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo2App.swift
+//  Demo2
+//
+//  Created by 유재환 on 8/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
